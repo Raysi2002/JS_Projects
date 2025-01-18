6d9a7203ff2c5413720fb61bd7ee90e2cf6be9f0
Aashish Kumar Ray <2002raysi@gmail.com>
2025-01-18 14:51:52 +0530

    Stopwatch styling

---
+++ basic projects/stopwatch/app.js
@@ -0,0 +1,7 @@
+const time = document.getElementById("time");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+
+
