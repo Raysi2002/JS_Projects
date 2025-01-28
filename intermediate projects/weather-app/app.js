ae4eeefc87fef5b5b1d616fd93829494855c5d4b
Aashish Kumar Ray <2002raysi@gmail.com>
2025-01-28 19:09:58 +0530

    Weather App

---
+++ intermediate projects/weather-app/app.js
@@ -0,0 +1,27 @@
+const apiKey = `8a66ceb846ec064f73cf62fa240272c3`;
+const searchBtn = document.getElementById(`search`);
+const cityName = document.getElementById('city-display');
+
+const weatherTemp = document.getElementById(`temp`);
+const weatherHumidity = document.getElementById(`humidity`);
+const weatherWindSpeed = document.getElementById(`wind`);
+
+async function getWeather(){
+    const city = document.getElementById(`city`).value;
+    if(!city)
+        alert(`${city} isn't a valid city`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    const weatherDetails = await response.json();
+
+    cityName.textContent = city;
+    weatherTemp.textContent = weatherDetails.main.temp;
+    weatherHumidity.textContent = weatherDetails.main.humidity;
+    weatherWindSpeed.textContent = weatherDetails.wind.speed;
+}
+
+searchBtn.addEventListener(`click`, getWeather);
+searchBtn.addEventListener(`keypress`, (e) => {
+    e.preventDefault();
+    if(e.key === `Enter`)
+        getWeather();
+})
