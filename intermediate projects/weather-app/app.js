2e34bbe00b7f7f46f3f459449ff842777f897967
Aashish Kumar Ray <2002raysi@gmail.com>
2025-01-28 15:58:14 +0530

    Weather APP

--- intermediate projects/weather-app/app.js
+++ intermediate projects/weather-app/app.js
@@ -1,3 +1,27 @@
-const apiKey = "";
-const city = document.getElementById("city");
-const tempLine = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}`;
+const apiKey = "8a66ceb846ec064f73cf62fa240272c3";
+const searchBtn = document.getElementById("search");
+const cityDisplay = document.getElementById("city-display");
+
+const temp = document.getElementById('temp');
+const wind = document.getElementById('wind-flow');
+const humidity = document.getElementById("humidity");
+
+async function cityWeather() {
+    const city = document.getElementById("city").value;
+    if(!city){
+        alert("Invalid city");
+    }
+    const tempLine = await `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    try{
+        const response = await fetch(tempLine);
+        if(!response.ok){
+            alert("No response returned");
+        }
+        const weatherDetails = await response.json();
+        
+    }catch(error){
+        console.error(error);
+    }
+}
+
+searchBtn.addEventListener('click', cityWeather);
